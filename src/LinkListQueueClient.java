74038092a731bc800c456671eec6d0075f16334f
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-04-03 15:56:06 +0700

    Add files

---
+++ src/LinkListQueueClient.java
@@ -0,0 +1,14 @@
+public class LinkListQueueClient {
+
+    public static void main(String[] args) {
+        QueueLinkedList queue = new QueueLinkedList();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.println("Dequeue item is " + queue.dequeue());
+    }
+}
---
+++ src/QueueLinkedList.java
@@ -0,0 +1,40 @@
+public class QueueLinkedList {
+    private Node head;
+    private Node tail;
+
+    public QueueLinkedList(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void enqueue(int key){
+        Node temp = new Node(key);
+        if (this.tail == null){
+            this.head = this.tail = temp;
+        }
+        this.tail.next = temp;
+        this.tail = temp;
+    }
+
+    public Node dequeue(){
+        if (this.head == null){
+            return null;
+        }
+        Node temp = this.head;
+        this.head = this.head.next;
+        if (this.head == null){
+            this.tail = null;
+        }
+        return temp;
+    }
+
+    class Node {
+        public int key;
+        public Node next;
+
+        public Node(int key){
+            this.key = key;
+            this.next = null;
+        }
+    }
+}
